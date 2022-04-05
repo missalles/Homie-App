00fb4c1378a6d5207d5547b692c704cb6c83e472
Lydia Alles <lydia-alles@web.de>
2022-04-05 12:48:45 +0200

    update

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,6 +3,11 @@ class PagesController < ApplicationController
   end
 
   def contact
+    @members = [ 'thanh', 'dimitri', 'germain', 'damien', 'julien' ]
+
+    if params[:member]
+        @members = @members.select { |member| member.start_with?(params[:member]) }
+    end
   end
 
   def home
